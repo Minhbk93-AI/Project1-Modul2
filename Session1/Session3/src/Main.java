568d155cb8a99add184a5f51745ec8771d63087a
Minhbk93-AI <minhnguyenbk93@gmail.com>
2024-08-02 15:28:36 +0700

    Update

---
+++ Session1/Session3/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int So;
+        String name;
+        Scanner sc= new Scanner(System.in);
+
+
+        System.out.println("Enter your name: ");
+        name = sc.nextLine();
+
+        System.out.println("Hello: " + name);
+        }
+
+}
